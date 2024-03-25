e3e484b013efaae09b3c5c426965ff587ed75a1e
AnubhavHawk <hawkanubhav@gmail.com>
2024-03-25 09:43:50 +0530

    Added tests

--- src/main/java/co/setu/splitwise/service/SplitService.java
+++ src/main/java/co/setu/splitwise/service/SplitService.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Service;
 public class SplitService {
 
     public static double calculateSplit(Double totalAmount, int totalUsers) {
+        if(totalAmount == 0) {
+            throw new IllegalArgumentException("Amount should be greater than zero");
+        }
+        if(totalUsers == 0) {
+            throw new IllegalArgumentException("Users should be more than zero");
+        }
         return totalAmount/totalUsers;
     }
 }
